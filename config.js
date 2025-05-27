d715f48d5174616e53a0865f9e404eaf713e9d6a
Shuukaa <69614231+Shuukaa@users.noreply.github.com>
2025-05-26 19:19:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     localStoragePrefix: "party-rank-sorter",
     title: "Noms 11 Party Rank Sorter",
-    description: "All Time Favorites [Set 2] Party rank sorter for your list of songs."
+    description: "Noms 11 Party rank sorter for your list of songs."
 };
